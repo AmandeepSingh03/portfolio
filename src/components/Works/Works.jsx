809fb8e09a8fb3a62af9dfe943760275a9943d3b
amandeepsingh03 <karan11750@gmail.com>
2023-03-26 02:32:15 +0530

    portfolio

---
+++ src/components/Works/Works.jsx
@@ -0,0 +1,69 @@
+import React, { useContext } from "react";
+import "./Works.css";
+import ckm from "../../img/ckm.jpg";
+import juspay from "../../img/juspay.jpg";
+import Amazon from "../../img/amazon.png";
+import iit from "../../img/iit.jpg";
+import cool from "../../img/glasses.png";
+import { themeContext } from "../../Context";
+import { motion } from "framer-motion";
+import {Link} from 'react-scroll'
+const Works = () => {
+  // context
+  const theme = useContext(themeContext);
+  const darkMode = theme.state.darkMode;
+
+  // transition
+  return (
+    <div className="works" id="works">
+      {/* left side */}
+      <div className="w-left">
+        <div className="awesome">
+          {/* dark Mode */}
+          <span style={{ color: darkMode ? "white" : "" }}>
+            Works for All these
+          </span>
+          <span>Brands & Clients</span>
+
+          <Link to="contact" smooth={true} spy={true}>
+            <button className="button s-button">Hire Me</button>
+          </Link>
+          <div
+            className="blur s-blur1"
+            style={{ background: "#ABF1FF94" }}
+          ></div>
+        </div>
+
+        {/* right side */}
+      </div>
+      <div className="w-right">
+        <motion.div
+          initial={{ rotate: 45 }}
+          whileInView={{ rotate: 0 }}
+          viewport={{ margin: "-40px" }}
+          transition={{ duration: 3.5, type: "spring" }}
+          className="w-mainCircle"
+        >
+          <div className="w-secCircle">
+            <img src={ckm} alt="" />
+          </div>
+          <div className="w-longCircle">
+            <img  src={juspay} alt="" />
+          </div>
+           {" "}
+          <div className="w-secCircle">
+            <img src={iit} alt="" />
+          </div>
+          <div className="w-Circle">
+            <img src={cool} alt="" />
+          </div>
+        </motion.div>
+        {/* background Circles */}
+        <div className="w-backCircle blueCircle"></div>
+        <div className="w-backCircle yellowCircle"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Works;
